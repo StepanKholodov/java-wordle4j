e2bde2e41c2585c12fa36d3505443cc7d47410c1
Stepan Kholodov <kholodov_stepan@inbox.ru>
2026-04-14 12:45:51 +0300

    Сделать загрузчик словаря

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -1,9 +1,39 @@
 package ru.yandex.practicum;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
     ему нужны методы по загрузке списка слов из файла по имени файла
     на выходе должен быть класс WordleDictionary
  */
 public class WordleDictionaryLoader {
+    private final String fileName;
+    private final PrintWriter logger;
+
+    public WordleDictionaryLoader(String fileName, PrintWriter logger) {
+        this.fileName = fileName;
+        this.logger = logger;
+    }
+
+
+    public WordleDictionary loadWords() throws IOException{
+        logger.println("Начало загрузки словаря из файла: " + fileName);
+        WordleDictionary wordleDictionary = new WordleDictionary(new ArrayList<>());
+        int count = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new FileReader(fileName, StandardCharsets.UTF_8))) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                wordleDictionary.addWord(line);
+                count++;
+            }
+
+        }
+        logger.println("Загружено слов: " + count);
+        return wordleDictionary;
+    }
 }
